490584c9c784c40510dae9de354db9bcfad2cc3a
Your Name <hydras15@gmail.com>
2024-09-13 15:07:52 -0500

    Tutorial for Creating A Counter using Phoenix LiveView

---
+++ lib/counter_web/live/counter.ex
@@ -0,0 +1,35 @@
+defmodule CounterWeb.Counter do
+  use CounterWeb, :live_view
+  alias Counter.Count
+  alias Phoenix.PubSub
+
+
+  @topic Count.topic
+
+  @spec mount(any(), any(), any()) :: {:ok, any()}
+  def mount(_params, _session, socket) do
+    PubSub.subscribe(Counter.PubSub, @topic)
+
+    {:ok, assign(socket, val: Count.current()) }
+  end
+
+  def handle_event("inc", _, socket) do
+    {:noreply, assign(socket, :val, Count.incr())}
+  end
+
+  def handle_event("dec", _, socket) do
+    {:noreply, assign(socket, :val, Count.decr())}
+  end
+
+  def handle_info({:count, count}, socket) do
+    {:noreply, assign(socket, val: count)}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <div>
+      <.live_component module={CounterComponent} id="counter" val={@val} />
+    </div>
+    """
+  end
+end
